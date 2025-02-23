1ab550832ec5b4797baef82276b08c4c5fc0bec7
unika1 <unikaprj@gmail.com>
2025-02-23 17:30:48 +0545

    changes

--- app/src/main/java/com/example/newproject/model/RemedyModel.kt
+++ app/src/main/java/com/example/newproject/model/RemedyModel.kt
@@ -1,4 +1,45 @@
 package com.example.newproject.model
 
-class RemedyModel {
+import android.os.Parcel
+import android.os.Parcelable
+
+class RemedyModel(
+    var remedyId: String = "",
+    var title: String = "",
+    var description: String = "",
+    var procedure: String = "",
+    var category: String = "", // Hair Care, Face Care, Body Care
+    var imageUrl: String = "", // Cloudinary Image URL
+) : Parcelable {
+    constructor(parcel: Parcel) : this (
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(remedyId)
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeString(procedure)
+        parcel.writeString(category)
+        parcel.writeString(imageUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RemedyModel> {
+        override fun createFromParcel(parcel: Parcel): RemedyModel {
+            return RemedyModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RemedyModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
