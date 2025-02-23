395a46b8cec33675fc51ce9a4f76a97557804382
unika1 <unikaprj@gmail.com>
2025-02-23 17:38:26 +0545

    changes

--- app/src/main/java/com/example/newproject/repository/RemedyRepository.kt
+++ app/src/main/java/com/example/newproject/repository/RemedyRepository.kt
@@ -1,4 +1,11 @@
 package com.example.newproject.repository
 
+import com.example.newproject.model.RemedyModel
+import com.google.firebase.database.DatabaseReference
+
 interface RemedyRepository {
+    fun addRemedy(remedy: RemedyModel, database: DatabaseReference, callback: (Boolean, String?) -> Unit)
+    fun getRemedies(database: DatabaseReference, callback: (List<RemedyModel>?, String?) -> Unit)
+    fun updateRemedy(remedy: RemedyModel, database: DatabaseReference, callback: (Boolean, String?) -> Unit)
+    fun deleteRemedy(remedyId: String, database: DatabaseReference, callback: (Boolean, String?) -> Unit)
 }
